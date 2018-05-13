ab32db10fd932af6b0a2139589cd994ae1dbb63d
Your Name <9leon1@gmail.com>
2018-05-13 21:54:04 +0300

    acordeon

---
+++ acordeon/src/lessons/15-Button/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+    render() {
+        return (
+
+            <div className="form">
+                {/*<img src="logo.png"/>*/}
+                <button>Увійти за допомогою Ehealth</button>
+            </div>
+        )
+    }
+}
+
+export default App;
